683f84a1ce08b90a5ea3399b05301da9d294a2ea
Ihor Lukianov <lis05st@gmail.com>
2025-11-03 15:56:28 +0100

    removed native diff for now. will go with the diff/patch

--- src/include/diff.hpp
+++ src/include/diff.hpp
@@ -28,38 +28,3 @@ public:
     virtual std::vector<std::byte> apply(const std::vector<std::byte> &data) = 0;
 };
 
-class NativeDiff : public Diff {
-private:
-    enum RecordType : uint8_t {
-        ADD,
-        DELETE,
-    };
-
-    class Record {
-    public:
-        RecordType type;
-        uint64_t   pos; /* Location in the source data. */
-
-        /* If type is DELETE, data will be empty, and len will contain the length. */
-        std::variant<std::vector<std::byte>, size_t> data;
-
-        Record(RecordType type, uint64_t pos);
-    };
-
-private:
-    std::vector<Record> changes;
-
-    /*
-     * Populate ::changes. Returns 0 on success.
-     */
-    int calculate(size_t src_size, std::byte *src_data, size_t dest_size,
-                  std::byte *dest_data);
-
-public:
-    NativeDiff() = default;
-
-    int from_files(const std::string &src, const std::string &dest) override;
-    std::vector<std::byte> binary_representation() override;
-    int from_binary_representation(const std::vector<std::byte> &data) override;
-    std::vector<std::byte> apply(const std::vector<std::byte> &data);
-};
